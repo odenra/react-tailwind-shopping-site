71d395df1f16b46d942e2647c07bd829cbb4555e
odenra <ilkerkaraoden@gmail.com>
2024-04-05 10:24:14 +0300

    product card done

---
+++ src/Components/PopularProductCard.jsx
@@ -0,0 +1,23 @@
+import { star } from "../assets/icons"
+
+const PopularProductCard = ({ imgURL, name, price }) => {
+  return (
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+        <img src={imgURL}
+            className="w-[280px] h-[280px] cursor-pointer"
+        />
+
+        <div className="mt-8 flex justify-start gap-2.5">
+            <img src={star}
+                width={24} height={24}
+            />
+            <p className="font-montserrat text-xl text-slate-gray ">4.5</p>
+        </div>
+
+        <h3 className="font-montserrat  font-semibold text-l mt-2 cursor-pointer">{name}</h3>
+        <p className="text-l mt-2">{price}</p>
+    </div>
+  )
+}
+
+export default PopularProductCard
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,6 +1,32 @@
+import PopularProductCard from '../Components/PopularProductCard';
+import { products } from '../const';
+
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+    <section id='products'
+             className='max-container
+                        max-sm:mt-12'>
+    
+    <div className='flex justify-start flex-col gap-5'>
+        <h2 className='text-4xl font-palanquin font-bold'>Our <span className='text-coral-red'>Popular</span> Products</h2>
+        <p className='lg:max-w-lg mt-2 font-montserrat text-slate-gray'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, praesentium!
+        </p>
+    </div>                        
+    
+    <div className='mt-16 grid
+                    lg:grid-cols-4 md:grid-cols-3
+                    sm:grid-cols-2 grid-cols-1
+                    sm:gap-4 gap-14'>
+
+
+    {products.map((product) => (
+      <PopularProductCard key= {product.name} {...product} />
+    ))}                  
+
+    </div>
+
+    </section>
   )
 }
 
