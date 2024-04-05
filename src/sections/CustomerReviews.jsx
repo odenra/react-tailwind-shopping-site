d0fe72be5aedb4e7c2ae742a761f2a4950e871f2
odenra <ilkerkaraoden@gmail.com>
2024-04-05 13:52:29 +0300

    customer reviews done

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,6 +1,28 @@
+import ReviewCard from "../Components/ReviewCard"
+import { reviews } from "../const"
+
 const CustomerReviews = () => {
   return (
-    <div>asdsad</div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text4xl font-bold">
+        What Our 
+        <span className="text-coral-red">Customers </span>
+        Say?
+      </h3>
+      <p className="info-text mt-4  m-auto max-w-lg text-center">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, nam in!
+      </p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard  key={review.customerName}
+                       imgURL={review.imgURL}
+                       custumerName={review.customerName}
+                       rating={review.rating}
+                       feedback={review.feedback}/>
+        ))}
+      </div>
+    </section>
   )
 }
 
